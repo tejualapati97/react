1436e8d6b931b5335933e84a5866bd22c260abf5
tejaswini <tejualapati2205@gmail.com>
2024-06-18 18:25:35 +0530

    timer application is done

---
+++ practise/src/components/Timer.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [int, setInt] = useState(null);
+
+  const HandleInc = () => {
+    clearInterval(int)
+    const interval = setInterval(()=>{
+      setTime((prev)=> prev + 1)
+    },1000)
+    setInt(interval)
+  };
+  const HandleDec = () => {
+    clearInterval(int)
+    const interval = setInterval(()=>{
+      setTime((prev)=> prev - 1)
+    },1000)
+    setInt(interval)
+  };
+  const HandlePause = () => {
+    clearInterval(int)
+    setTime(time)
+  };
+  const HandleReset = () => {
+    clearInterval(int)
+    setTime(0)
+  };
+
+  return (
+    <div>
+      <h1>{time}</h1>
+      <button onClick={HandleInc}>Auto Inc</button>&nbsp;
+      <button onClick={HandleDec}>Auto Dec</button>&nbsp;
+      <button onClick={HandlePause}>Pause</button>&nbsp;
+      <button onClick={HandleReset}>Reset</button>&nbsp;
+    </div>
+  );
+};
+
+export default Timer;
